20d20c2846e1edc40f3831b0d817201a7b5d646d
miguel rivera <miguelmjro@gmail.com>
2021-07-27 18:15:19 -0500

    adding all

---
+++ app.js
@@ -0,0 +1,19 @@
+const port = 3000;
+
+var express = require('express');
+let fs = require('fs');
+var app = express();
+
+
+app.use(express.static(__dirname + '/resources'));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+app.listen(port);
+console.log(port + '3000 is the magic port');
